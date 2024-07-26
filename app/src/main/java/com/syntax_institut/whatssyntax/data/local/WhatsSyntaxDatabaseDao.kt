81d47c301f5b6a1f70ecc4133ae0a86155922aae
Maxim Svidrak <maximsvid@gmail.com>
2024-07-26 10:40:31 +0200

    version 4.2 database 26.07.24

--- app/src/main/java/com/syntax_institut/whatssyntax/data/local/WhatsSyntaxDatabaseDao.kt
+++ app/src/main/java/com/syntax_institut/whatssyntax/data/local/WhatsSyntaxDatabaseDao.kt
@@ -1,8 +1,23 @@
 package com.syntax_institut.whatssyntax.data.local
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.syntax_institut.whatssyntax.model.NotesData
 
 @Dao
 interface WhatsSyntaxDatabaseDao {
+
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(notes: NotesData)
+
+    @Delete
+    suspend fun delete(note: NotesData)
+
+    @Query("SELECT * FROM notes_table")
+    fun getAll(): LiveData<List<NotesData>>
 
 }
